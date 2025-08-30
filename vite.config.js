5a135b21e3494e955b39255073620153ba44bd1c
Richard <richardguo24@gmail.com>
2025-08-30 19:22:11 -0400

    changed vite.config.js so that vercel works

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-const BASE_PROD = '/REPO_NAME/'
-// https://vite.dev/config/
-export default defineConfig(({ mode }) =>
-({
-  base: mode === 'production' ? BASE_PROD : '/',
+
+export default defineConfig({
+  base: '/',            // important: no repo subpath on Vercel
   plugins: [react()],
-}))
+})
